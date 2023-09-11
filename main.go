98773d82e31a9072e9317ab7853290287f4a81b2
CrimsonCoder42 <nostro37@gmail.com>
2023-09-11 13:52:56 -0400

    file save  retrieve

--- main.go
+++ main.go
@@ -1,17 +1,8 @@
 package main
 
 func main() {
-
-	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+	cards := newDeckFromFile("m_cards")
+	cards.print()
 
 
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
